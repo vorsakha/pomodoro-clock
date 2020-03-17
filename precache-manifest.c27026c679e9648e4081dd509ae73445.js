61ac263d44405bf9032aa878b6dd88632dc864e4
tsaristbomba <andrexerdna@gmail.com>
2020-03-17 09:42:24 -0300

    Updates

---
+++ precache-manifest.c27026c679e9648e4081dd509ae73445.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "449a0d055ed857004072642b9bf8e792",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "31df94d391389dabde18",
+    "url": "/pomodoro-clock/static/css/main.d6bd4328.chunk.css"
+  },
+  {
+    "revision": "8fca4b82b2d2a492bb88",
+    "url": "/pomodoro-clock/static/js/2.e2146c02.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pomodoro-clock/static/js/2.e2146c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31df94d391389dabde18",
+    "url": "/pomodoro-clock/static/js/main.5a515f97.chunk.js"
+  },
+  {
+    "revision": "4cc8e959fe1d967d61ff",
+    "url": "/pomodoro-clock/static/js/runtime-main.c524c67b.js"
+  }
+]);
